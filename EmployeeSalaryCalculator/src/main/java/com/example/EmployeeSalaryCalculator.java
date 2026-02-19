d4bc437e592c2ba019736f86eec4cfbeaf5ff571
Christian Julian Pili <christian.julian.pili@gmail.com>
2026-02-19 16:06:18 -0500

    Salary Calculator assignment completed

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -59,15 +59,21 @@ public class EmployeeSalaryCalculator
 				if(HW <= standardWorkingHours)
 				{
 					//YOUR CODE STARTS HERE
-					 
-
+					 overTime = 0;
+					 overTimeRate = 0;
+					 overTimePay = 0;
+					 basicPay = HW * HR;
+					 grossSalary = basicPay;
 					//YOUR CODE ENDS HERE
 				}
 				else
 				{
 					//YOUR CODE STARTS HERE
- 
-
+ 					overTime = HW - standardWorkingHours;
+					overTimeRate = HR * 2;//Overtime work = double rate
+					overTimePay = overTime * overTimeRate;
+					basicPay = standardWorkingHours * HR;
+					grossSalary = basicPay + overTimePay;
 					//YOUR CODE ENDS HERE
 				}
 				return grossSalary;
